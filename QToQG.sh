23ae853f907a10b48252f99f15d202fc06c62003
ismail <isma@physik.uni-bielefeld.de>
2021-11-26 13:58:11 -0500

    First commit

--- QToQG.sh
+++ QToQG.sh
@@ -5,24 +5,24 @@ MakeFile="PlotMaking/MakefileQToGQ"
 P="300" # 300xTemperature
 z="0.25"
 
-mkdir PlotMaking/$Process
+mkdir -p PlotMaking/$Process
 
 make -f $MakeFile OpacityRate
 ./OpacityRate.out -P $P -z $z
-mkdir PlotMaking/$Process/Opacity
+mkdir -p PlotMaking/$Process/Opacity
 mv -f Opacity/Rate-P300-z0.25.txt PlotMaking/$Process/Opacity/
 
 make -f $MakeFile ImprovedOpacity
 ./ImprovedOpacity.out -P $P -z $z
-mkdir PlotMaking/$Process/OpacityImproved
+mkdir -p PlotMaking/$Process/OpacityImproved
 mv -f OpacityImproved/Rate-P300-z0.25.txt PlotMaking/$Process/OpacityImproved/
 
 make -f $MakeFile Harmonic
 ./HO.out -P $P -z $z
-mkdir PlotMaking/$Process/HO
+mkdir -p PlotMaking/$Process/HO
 mv -f HO/Rate-P300-z0.25.txt PlotMaking/$Process/HO/
 
 make -f $MakeFile FullRate
 ./FullRate.out -P $P -z $z
-mkdir PlotMaking/$Process/FullRate
+mkdir -p PlotMaking/$Process/FullRate
 mv -f OUTPUT/Rate-P300-z0.25.txt PlotMaking/$Process/FullRate/
